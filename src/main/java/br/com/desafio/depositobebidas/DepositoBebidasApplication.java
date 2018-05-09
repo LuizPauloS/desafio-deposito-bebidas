ce847a2fc23c7b2491bc66b7e522d14151fd31f0
Luiz Paulo da Silva <luiz.silva@nastek.local>
2018-05-09 08:28:58 -0400

    Commit inicial desafio

---
+++ src/main/java/br/com/desafio/depositobebidas/DepositoBebidasApplication.java
@@ -0,0 +1,12 @@
+package br.com.desafio.depositobebidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoBebidasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepositoBebidasApplication.class, args);
+    }
+}
