c6ce9841b65b5bcd5ab2ddec4fbaf240cf92faf8
luiz.silva <luiz.silva@NB-DEV-05.nastek.local>
2018-05-14 16:07:54 -0400

    Adicionando anotações da documentação com swagger em recursos de registro e seção.

--- src/main/java/br/com/desafio/depositobebidas/resource/RegistroResource.java
+++ src/main/java/br/com/desafio/depositobebidas/resource/RegistroResource.java
@@ -1,7 +1,11 @@
 package br.com.desafio.depositobebidas.resource;
 
+import br.com.desafio.depositobebidas.model.Bebida;
 import br.com.desafio.depositobebidas.model.Registro;
 import br.com.desafio.depositobebidas.repository.RegistroRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +22,11 @@ public class RegistroResource {
     private RegistroRepository registroRepository;
 
     @GetMapping
+    @ApiOperation(value = "Recurso para listar todos os registros.", response = Registro.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna a lista de registros."),
+        @ApiResponse(code = 204, message = "Sem conteúdo para retornar!")})
     public ResponseEntity<List<Registro>> getAllRegistros() {
         List<Registro> registros = this.registroRepository.findAll();
         if (!registros.isEmpty()) {
@@ -27,6 +36,11 @@ public class RegistroResource {
     }
 
     @GetMapping(value = "/entrada")
+    @ApiOperation(value = "Recurso para listar registros de entrada.", response = Bebida.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna a lista de registros entrada."),
+        @ApiResponse(code = 204, message = "Sem conteúdo para retornar!")})
     public ResponseEntity<List<Registro>> getAllRegistrosEntrada() {
         List<Registro> registrosEntrada = this.registroRepository.findRegistrosEntradaOrByDataAndSecao();
         if (!registrosEntrada.isEmpty()) {
@@ -36,6 +50,11 @@ public class RegistroResource {
     }
 
     @GetMapping(value = "/saida")
+    @ApiOperation(value = "Recurso para listar registros de saída.", response = Bebida.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna a lista de registros saída."),
+        @ApiResponse(code = 204, message = "Sem conteúdo para retornar!")})
     public ResponseEntity<List<Registro>> getAllRegistrosSaida() {
         List<Registro> registrosSaida = this.registroRepository.findRegistrosSaidaOrByDataAndSecao();
         if (!registrosSaida.isEmpty()) {
--- src/main/java/br/com/desafio/depositobebidas/resource/SecaoResource.java
+++ src/main/java/br/com/desafio/depositobebidas/resource/SecaoResource.java
@@ -1,7 +1,11 @@
 package br.com.desafio.depositobebidas.resource;
 
+import br.com.desafio.depositobebidas.model.Registro;
 import br.com.desafio.depositobebidas.model.Secao;
 import br.com.desafio.depositobebidas.service.SecaoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +24,11 @@ public class SecaoResource {
     private SecaoService secaoService;
 
     @GetMapping
+    @ApiOperation(value = "Recurso para listar todas as seções.", response = Registro.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna a lista de seções."),
+        @ApiResponse(code = 204, message = "Sem conteúdo para retornar!")})
     public ResponseEntity<List<Secao>> getAllSecoes() {
         List<Secao> estoque = this.secaoService.buscaTodasSecoes();
         if (estoque != null && !estoque.isEmpty()) {
@@ -29,6 +38,11 @@ public class SecaoResource {
     }
 
     @PostMapping(value = "/cadastro")
+    @ApiOperation(value = "Recurso para cadastrar seção.", response = Registro.class,
+            responseContainer = "List")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Retorna objeto json da seção criada."),
+        @ApiResponse(code = 204, message = "Sem conteúdo para retornar!")})
     public ResponseEntity<Secao> addSecaoEstoque(@RequestBody Secao secao) {
         if (secao != null) {
             Secao s = this.secaoService.adicionaSecao(secao);
