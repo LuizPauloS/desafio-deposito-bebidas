9034b6091afd9f5d1466aeec74bc138b1db07bed
luiz.silva <luiz.silva@NB-DEV-05.nastek.local>
2018-05-14 16:06:27 -0400

    Corrigindo queries de seções.

--- src/main/java/br/com/desafio/depositobebidas/repository/SecaoRepository.java
+++ src/main/java/br/com/desafio/depositobebidas/repository/SecaoRepository.java
@@ -1,9 +1,23 @@
 package br.com.desafio.depositobebidas.repository;
 
 import br.com.desafio.depositobebidas.model.Secao;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface SecaoRepository extends JpaRepository<Secao, Long>{
- 
-    Secao findByNome(String nome);
+public interface SecaoRepository extends JpaRepository<Secao, Long> {
+
+    Secao findSecaoById(Long id);
+    
+    Secao findSecaoByNome(String nome);
+
+    @Query("SELECT SUM(volume) FROM Secao")
+    Double findVolumeTotalEstoque();
+
+    @Query("SELECT SUM(volume) FROM Secao WHERE bebida_id = ?1")
+    Double findVolumeTotalEstoqueByTipoBebida(@Param("id") Long id);
+
+    @Query("SELECT s FROM Secao s WHERE bebida_id IS NULL OR volume = 0")
+    List<Secao> findSecoesDisponiveis();
 }
